1e4c83afbe1c87983276c5e3612d0c75cd697496
AgusMertha <agusmertha.am@gmail.com>
2020-12-12 11:48:02 +0800

    setup model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['status', 'user_id', 'course_id', 'metadata', 'snap_url'];
+
+    protected $casts = [
+        "created_at" => "datetime:Y-m-d H:m:s",
+        "updated_at" => "datetime:Y-m-d H:m:s",
+        "metadata" => "array"
+    ];
 }
--- app/Models/PaymentLog.php
+++ app/Models/PaymentLog.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['status', 'payment_type', 'raw_response', 'order_id'];
+
+    protected $casts = [
+        "created_at" => "datetime:Y-m-d H:m:s",
+        "updated_at" => "datetime:Y-m-d H:m:s"
+    ];
 }
